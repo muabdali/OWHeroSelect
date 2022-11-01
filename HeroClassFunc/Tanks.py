91d37d387d9ea3a1bdd6279121a3e686d0cdfe85
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-10-31 23:57:27 -0400

    tanks py

---
+++ HeroClassFunc/Tanks.py
@@ -0,0 +1,58 @@
+import pyautogui
+def DVASelect():
+    print("DVA Selected")
+    pyautogui.click(350, 833)
+    pyautogui.click(350, 833)
+
+
+
+def DoomSelect():
+    print("Doomfist Selected")
+    pyautogui.click(411, 834)
+    pyautogui.click(411, 834)
+
+
+
+def JunkerSelect():
+    print("Junker Queen Selected")
+    pyautogui.click(467, 834)
+    pyautogui.click(467, 834)
+
+def OrisaSelect():
+    print("Orisa Selected")
+    pyautogui.click(543, 843)
+    pyautogui.click(543, 843)
+
+def ReinSelect():
+    print("Reinhardt Selected")
+    pyautogui.click(600, 836)
+    pyautogui.click(600, 836)
+
+def HogSelect():
+    print("RoadHog Selected")
+    pyautogui.click(347, 899)
+    pyautogui.click(347, 899)
+
+def SigmaSelect():
+    print("Sigma Selected")
+    pyautogui.click(411, 889)
+    pyautogui.click(411, 889)
+
+def WinstonSelect():
+    print("Winston Selected")
+    pyautogui.click(478, 889)
+    pyautogui.click(478, 889)
+
+def HamsterSelect():
+    print("Hamster Selected")
+    pyautogui.click(540, 899)
+    pyautogui.click(540, 899)
+
+def ZaryaSelect():
+    print("Zarya Selected")
+    pyautogui.click(600 , 899)
+    pyautogui.click(600 , 899)
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 import pyautogui
 
-for i in range(25):
-    pyautogui.click(350,825)
-    pyautogui.click(350,825)
-
-# TEST
