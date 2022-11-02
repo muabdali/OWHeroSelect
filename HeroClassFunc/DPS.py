acebc9ab9763515dfb89801912c198e987beff0d
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-11-01 20:04:52 -0400

    dps and support finished, all thats left is the gui and maybe game start detection

---
+++ HeroClassFunc/DPS.py
@@ -0,0 +1,85 @@
+import pyautogui
+
+
+def AsheSelect():
+    print("Ashe Selected")
+    pyautogui.click(762, 829)
+    pyautogui.click(762, 829)
+
+def BastionSelect():
+    print("DVA Selected")
+    pyautogui.click(831 , 829)
+    pyautogui.click(831 , 829)
+
+def McCreeSelect():
+    print("McCree Selected")
+    pyautogui.click(904, 829)
+    pyautogui.click(904, 829)
+
+def EchoSelect():
+    print("Echo Selected")
+    pyautogui.click(956, 829)
+    pyautogui.click(956, 829)
+
+def GenjiSelect():
+    print("Genji Selected")
+    pyautogui.click(1016, 829)
+    pyautogui.click(1016, 829)
+
+def HanzoSelect():
+    print("Hanzo Selected")
+    pyautogui.click(1091, 829)
+    pyautogui.click(1091, 829)
+
+def JunkratSelect():
+    print("Junkrat Selected")
+    pyautogui.click(1150, 829)
+    pyautogui.click(1150, 829)
+
+def PharahSelect():
+    print("Pharah Selected")
+    pyautogui.click(1220, 829)
+    pyautogui.click(1220, 829)
+
+def ReaperSelect():
+    print("Reaper Selected")
+    pyautogui.click(769 , 899)
+    pyautogui.click(769 , 899)
+
+def ShootyGirlSelect():
+    print("Shooty Girl Selected")
+    pyautogui.click(830, 899)
+    pyautogui.click(830, 899)
+
+def SoldierSelect():
+    print("Soldier Selected")
+    pyautogui.click(900, 899)
+    pyautogui.click(900, 899)
+
+
+def SombraSelect():
+    print("Sombra Selected")
+    pyautogui.click(960, 899)
+    pyautogui.click(960, 899)
+
+
+def TorbjornSelect():
+    print("Torbjorn Selected")
+    pyautogui.click(1085, 899)
+    pyautogui.click(1085, 899)
+
+def TracerSelect():
+    print("Tracer Selected")
+    pyautogui.click(1151, 899)
+    pyautogui.click(1151, 899)
+
+def WidowSelect():
+    print("Widow Selected")
+    pyautogui.click(1211, 899)
+    pyautogui.click(1211, 899)
+
+def SymettraSelect():
+    print("Symettra Selected")
+    pyautogui.click(1021, 899)
+    pyautogui.click(1021, 899)
+
---
+++ HeroClassFunc/Support.py
@@ -0,0 +1,42 @@
+import pyautogui
+
+def AnaSelect():
+    print("Ana Selected")
+    pyautogui.click(1381, 829)
+    pyautogui.click(1381, 829)
+
+def BriggetteSelect():
+    print("Briggette Selected")
+    pyautogui.click(1521 , 829)
+    pyautogui.click(1521 , 829)
+
+def LucioSelect():
+    print("Lucio Selected")
+    pyautogui.click(1368 , 899)
+    pyautogui.click(1368 , 899)
+
+def MercySelect():
+    print("Mercy Selected")
+    pyautogui.click(1452 , 899)
+    pyautogui.click(1452 , 899)
+
+def MoiraSelect():
+    print("Moira Selected")
+    pyautogui.click(1515, 899)
+    pyautogui.click(1515, 899)
+
+def ZenyattaSelect():
+    print("Zenyatta Selected")
+    pyautogui.click(1571, 899)
+    pyautogui.click(1571, 899)
+
+def ShootyGuySelect():
+    print("Shooty Guy Selected")
+    pyautogui.click(1450, 829)
+    pyautogui.click(1450, 829)
+
+def KurikoSelect():
+    print("Kuriko Selected")
+    pyautogui.click(1580 ,829)
+    pyautogui.click(1580 ,829)
+
